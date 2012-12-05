7b4ab12f6015a78c2e25cf2edef7eabd71bdc21b
root <sumit@grepruby.com>
2012-12-06 01:24:24 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vivah::Application.config.secret_token = 'd1d1527fa89a42938b28453cc80360648a6cd4cb3c450cf0333e0097ba2b6153cbc066647c0b0625fd985c5abb6c1f629736a0a78b5bdbc5116ce49325efc7da'
